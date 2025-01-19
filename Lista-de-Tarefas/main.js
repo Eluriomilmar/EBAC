64c871fc256a5bc3dfd10d7e4fde28d0bd741304
Eluriomilmar <lucasrfranca@hotmail.com>
2025-01-19 18:06:51 -0300

    Lista de Tarefas

--- Lista-de-Tarefas/main.js
+++ Lista-de-Tarefas/main.js
@@ -1,14 +1,21 @@
 $(document).ready(function(){
-    $("div").on("submit", function(e) {
+    $("form").on("submit", function(e) {
         e.preventDefault();
         const novaTarefa = $("#nome-tarefa").val();
         const novoItem = $("<li></li>");
-        $(`<div class="tarefas">
+        $(`<div class="tarefas" id=${novaTarefa}>
                 ${novaTarefa}
             </div>`).appendTo(novoItem);
         $(novoItem).appendTo("ul");
         $("#nome-tarefa").val("");
-            
-    })
+
+    $(".tarefas").on("click", function() {
+        const id = $(this).attr("id");
+        $(this).css({
+            textDecoration: "line-through"
+        });
+    });
+
+    });
 
 })
